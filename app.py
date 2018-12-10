39d54b7d02cc706843bf1a6ae57bef75066fa8f9
Alix <alix.mallard@gmail.com>
2018-12-10 18:25:54 +0100

    add apointment controller

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "app.db"))
 
-# Configurations
-app.config.from_object('config')
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 
 db = SQLAlchemy(app)
 
--- appointments.py
+++ appointments.py
@@ -13,3 +13,37 @@ class Appointment(db.Model):
         self.__date = date
         self.__care = care
 
+    @property
+    def nurse_id(self):
+        return self.__nurse_id
+
+    @nurse_id.setter
+    def nurse_id(self, id):
+        self.__nurse_id = id
+
+    @property
+    def patient_id(self):
+        return self.__patient_id
+
+    @patient_id.setter
+    def patient_id(self, id):
+        self.__patient_id = id
+
+    @property
+    def date(self):
+        return self.__date
+
+    @date.setter
+    def date(self, date):
+        self.__date = date
+
+    @property
+    def care(self):
+        return self.__care
+
+    @care.setter
+    def care(self, care):
+        self.__care = care
+
+
+
