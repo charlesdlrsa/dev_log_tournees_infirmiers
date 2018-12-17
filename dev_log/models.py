532974a4e460110d9340aeceda8c14c7067a47cb
Romain Pascual <romain.pascual@student.ecp.fr>
2018-12-17 16:24:17 +0100

    Ajout des relations

--- dev_log/models.py
+++ dev_log/models.py
@@ -3,78 +3,175 @@ from dev_log import db
 
 class Base(db.Model):
     __abstract__ = True
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(
+        db.Integer, 
+        primary_key=True, 
+        nullable=False)
 
 
 class BasePerson(Base):
     __abstract__ = True
-    last_name = db.Column(db.String(20))
-    first_name = db.Column(db.String(20))
-    email = db.Column(db.String(20))
-    phone = db.Column(db.String(10))
-    address = db.Column(db.String(50))
 
+    last_name = db.Column(
+        db.String(20), 
+        nullable=False)
+
+    first_name = db.Column(
+        db.String(20), 
+        nullable=False)
+
+    email = db.Column(
+        db.String(20), 
+        nullable=False)
+
+    phone = db.Column(
+        db.String(10), 
+        nullable=False)
+
+    address = db.Column(
+        db.String(50), 
+        nullable=False)
 
 class Appointment(Base):
     id = db.Column('appointment_id', db.Integer, primary_key=True)
     patient_id = db.Column(
-            db.Integer, 
-            db.ForeignKey('patient.id'), 
-            nullable=False, 
-            unique=True)
+        db.Integer, 
+        db.ForeignKey('patient.patient_id'), 
+        nullable=False, 
+        unique=True)
 
     nurse_id = db.Column(
         db.Integer, 
         db.ForeignKey('nurse.nurse_id'), 
+        nullable=False,
+        unique=True)
+
+    care_id = db.Column(
+        db.Integer, 
+        db.ForeignKey('care.care_id'), 
+        nullable=False, 
+        unique=True)
+
+    date = db.Column(
+        db.DateTime, 
         nullable=False)
 
-    date = db.Column(db.DateTime, nullable=False)
-    care_id = db.Column(
-            db.Integer, 
-            db.ForeignKey('care.care_id'), 
-            nullable=False, 
-            unique=True)
-
-    def __init__(self, nurse_id, patient_id, date, care):
-        self.nurse_id = nurse_id
-        self.patient_id = patient_id
-        self.date = date
-        self.care = care
+    def __init__(self, nurse_id, patient_id, date, care, phone):
+        self.__nurse_id = nurse_id
+        self.__patient_id = patient_id
+        self.__date = date
+        self.__care = care
+        self.__phone = phone
 
 
 
 class Nurse(BasePerson):
-    id = db.Column('nurse_id', db.Integer, primary_key=True)
-    password = db.Column(db.String(20))
-    office = db.Column(db.ARRAY(db.Integer, as_tuple=False))
+    id = db.Column(
+        'nurse_id', 
+        db.Integer, 
+        primary_key=True, 
+        nullable=False)
+
+    password = db.Column(
+        db.String(20), 
+        nullable=False)
+
     # competences = db.Column(db.String(50))
+    
+    appointments = db.relationship(
+        'Appointment')
+
+    office = db.relationship(
+        'AssociationOfficeNurse')
 
     def __init__(self, last_name, first_name, email, password, address, office):
-        self.last_name = last_name
-        self.first_name = first_name
-        self.email = email
-        self.password = password
-        self.address = address
-        self.office = office
+        self.__last_name = last_name
+        self.__first_name = first_name
+        self.__email = email
+        self.__password = password
+        self.__address = address
+        self.__office = office
         # self.competences = competences
 
 class Care(Base):
-    id = db.Column('care_id', db.Integer, primary_key=True)
-    description = db.Column(db.String(200))
-    duration = db.Column(db.Integer) # duration in minutes
+    id = db.Column(
+        'care_id', 
+        db.Integer, 
+        primary_key=True, 
+        nullable=False)
+
+    description = db.Column(
+        db.String(200))
+
+    duration = db.Column(# duration in minutes
+        db.Integer, 
+        nullable=False)
+    
+    appointments = db.relationship(
+        'Appointment')
 
     def __init__(self, description, duration):
         self.__description = description
         self.__duration = duration
 
+
 class Patient(BasePerson):
-    id = db.Column('patient_id', db.Integer, primary_key=True)
-    related_office = db.Column(db.Integer)
+    id = db.Column(
+        'patient_id', 
+        db.Integer, 
+        primary_key=True, 
+        nullable=False)
+    
+    appointments = db.relationship(
+            'Appointment')
 
-    def __init__(self, last_name, first_name, email, address, related_office):
-        self.last_name = last_name
-        self.first_name = first_name
-        self.email = email
-        self.address = address
-        self.related_office = related_office
+    def __init__(self, last_name, first_name, email, address, phone):
+        self.__last_name = last_name
+        self.__first_name = first_name
+        self.__email = email
+        self.__address = address
+        self.__phone = phone
 
+class Office(Base):
+    id = db.Column(
+        'office_id', 
+        db.Integer, 
+        primary_key=True, 
+        nullable=False)
+
+    name = db.Column(
+        db.String(50), 
+        nullable=False)
+
+    phone = db.Column(
+        db.String(10), 
+        nullable=False)
+
+    address = db.Column(
+        db.String(50), 
+        nullable=False)
+
+    nurses = db.relationship("AssociationOfficeNurse")
+
+    def __init__(self, name, phone, adress):
+        self.__name = name
+        self.__phone = phone
+        self.__address = adress
+
+# Many to Many relation
+class AssociationOfficeNurse(Base):
+    office_id = db.Column(
+        db.Integer,
+        db.ForeignKey('office.office_id'),
+        primary_key=True, 
+        nullable=False)
+
+    nurse_id = db.Column(
+        db.Integer,
+        db.ForeignKey('nurse.nurse_id'),
+        primary_key=True, 
+        nullable=False)
+
+    nurse = db.relationship("Nurse")
+
+    office = db.relationship("Office")
