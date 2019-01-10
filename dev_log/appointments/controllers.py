d3a554c6cf9f3c501132f679df8471ba9fc4b039
Alix <alix.mallard@gmail.com>
2019-01-10 15:25:42 +0100

    merge

--- dev_log/appointments/controllers.py
+++ dev_log/appointments/controllers.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for
-from werkzeug.security import check_password_hash, generate_password_hash
 
 from dev_log import db
 from dev_log.models import Appointment, Patient
@@ -24,11 +23,11 @@ def home():
         return redirect(url_for(get_appointments, last_name=last_name, first_name=first_name))
 
     appointments = Appointment.query.all()
-    return render_template(..., appointments=appointments)
+    return render_template('appointments.html', appointments=appointments)
 
 
-@appointments.route('/add', methods=['GET', 'POST'])
-def add_nurse():
+@appointments.route('/add_appointment', methods=['GET', 'POST'])
+def add_appointment():
     """
     Add a new appointment
     :return:
