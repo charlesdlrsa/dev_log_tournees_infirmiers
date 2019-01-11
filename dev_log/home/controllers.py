f4b5868b4e3116a8f95b341bf25327db6df45103
charlesdlrsa <charlesdlrsa@hotmail.fr>
2019-01-11 10:35:20 +0100

    Ajout de la fonction init_db et du boutton. Modification du controller appointment.

--- dev_log/home/controllers.py
+++ dev_log/home/controllers.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for
+from dev_log.models import init_db
 
 home = Blueprint('home', __name__)
 
 
-@home.route("/")
+@home.route("/", methods=['GET', 'POST'])
 def index():
+    if request.method == "POST":
+        init_db()
     return render_template("landing.html")
