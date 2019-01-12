65b5389cd8c07c72c6e0bab4861e299d453fdd43
Hippolyte Lévêque <hippolyte.leveque@student.ecp.fr>
2019-01-12 19:33:44 +0100

    Started calendar for global planning page

--- dev_log/home/controllers.py
+++ dev_log/home/controllers.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for
 from dev_log.models import init_db
+from datetime import datetime,timedelta
 
 home = Blueprint('home', __name__)
 
@@ -8,4 +9,16 @@ home = Blueprint('home', __name__)
 def index():
     if request.method == "POST":
         init_db()
-    return render_template("landing.html")
+    date = datetime.now()
+    start_week=date - timedelta(date.weekday())
+    end_week = start_week + timedelta(6)
+    start_week = str(start_week.day) + '/' + str(start_week.month)
+    end_week = str(end_week.day) + '/' + str(end_week.month)
+    return render_template("landing.html",
+    start_week=start_week,
+    end_week=end_week,
+    date=date)
+
+
+
+
