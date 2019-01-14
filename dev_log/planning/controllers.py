29e2ed390c4285a142f0f10f7f6f21705a5409a2
Hippolyte Lévêque <hippolyte.leveque@student.ecp.fr>
2019-01-14 16:05:42 +0100

    Calendar properly working on ex-landing page

--- dev_log/planning/controllers.py
+++ dev_log/planning/controllers.py
@@ -3,6 +3,9 @@ from dev_log.models import init_db
 from datetime import datetime,timedelta
 from dev_log.auth.controllers import login_required
 from dev_log.auth.controllers import admin_required
+from dev_log.utils.calendar import *
+from datetime import *
+from dev_log.models.calendar import *
 
 planning = Blueprint('planning', __name__, url_prefix='/planning')
 
@@ -11,13 +14,33 @@ planning = Blueprint('planning', __name__, url_prefix='/planning')
 @login_required
 def home():
     if request.method == "POST":
-        init_db()
-    date = datetime.now()
-    start_week=date - timedelta(date.weekday())
-    end_week = start_week + timedelta(6)
+        print('lol')
+    if "week" in request.args:
+        week=int(request.args['week'])
+        year=int(request.args['year'])
+        if week==0:
+            week=52
+            year=year-1
+        elif week==53:
+            week=1
+            year=year+1
+    else:
+        current_date = datetime.now()
+        week = current_date.isocalendar()[1]
+        year = current_date.isocalendar()[0]
+    start_week=iso_to_gregorian(year,week,1)
+    end_week=iso_to_gregorian(year,week,7)
     start_week = str(start_week.day) + '/' + str(start_week.month)
     end_week = str(end_week.day) + '/' + str(end_week.month)
-    return render_template("landing.html", start_week=start_week, end_week=end_week, date=date)
+    return render_template("landing.html",
+    start_week=start_week,
+    end_week=end_week,
+    year=year,
+    week=week)
+
+
+
+
 
 
 
