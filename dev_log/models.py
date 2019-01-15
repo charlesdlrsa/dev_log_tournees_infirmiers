fc709e4e5de4a790e576ee3487eb23b9778d52a0
charlesdlrsa <charlesdlrsa@hotmail.fr>
2019-01-15 10:39:21 +0100

    availabilities

--- dev_log/models.py
+++ dev_log/models.py
@@ -80,6 +80,10 @@ class Nurse(BasePerson):
         db.String(20),
         nullable=False)
 
+    cares = db.Column(
+        db.String(50),
+    )
+
     # competences = db.Column(db.String(50))
 
     def __init__(self, last_name, first_name, email, password,
@@ -232,13 +236,17 @@ def init_db():
     db.session.add(Office(name="Doctissimo", phone="0647859648", address="38 rue Lecourbe",
                           email="doctissimo@hotmail.fr", password=password))
     db.session.add(Nurse(last_name="Cabaret", first_name="Laurent", email="laurent.cabaret@hotmail.fr",
-                         phone="0699458758", password=password, address="35 rue Bobigny", office="Paris"))
+                         phone="0699458758", password=password, address="35 rue Bobigny", office="Paris",
+                         cares="1"))
     db.session.add(Nurse(last_name="Poly", first_name="Jean-Philippe", email="jpp@hotmail.fr",
-                         phone="0699458758", password=password, address="48 rue Clovis", office="Paris"))
+                         phone="0699458758", password=password, address="48 rue Clovis", office="Paris",
+                         cares="1"))
     db.session.add(Nurse(last_name="Hulot", first_name="Celine", email="celine.hulot@hotmail.fr",
-                         phone="0699469858", password=password, address="76 rue Paul André", office="Paris"))
+                         phone="0699469858", password=password, address="76 rue Paul André", office="Paris",
+                         cares="1"))
     db.session.add(Nurse(last_name="Detriche", first_name="Jean-Marie", email="jeanmarie.detriche@hotmail.fr",
-                         phone="0694699858", password=password, address="24 rue Terrence", office="Paris"))
+                         phone="0694699858", password=password, address="24 rue Terrence", office="Paris",
+                         cares="1"))
     db.session.add(Patient(last_name="De la roche", first_name="Charles", email="charles.dlrsa@hotmail.fr",
                            address="40 rue Victor Hugo", phone="0699497758"))
     db.session.add(Patient(last_name="Mallard", first_name="Alix", email="alix.mallard@hotmail.fr",
