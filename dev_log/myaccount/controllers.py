2062afe0456acc5aa3616c4380c332e43fe06328
Alix <alix.mallard@gmail.com>
2019-01-21 17:00:24 +0100

    absence

--- dev_log/myaccount/controllers.py
+++ dev_log/myaccount/controllers.py
@@ -1,57 +1,39 @@
 from flask import Blueprint, request, render_template, flash, redirect, url_for, session
 from werkzeug.security import check_password_hash, generate_password_hash
-import re
-from sqlalchemy.sql import or_
 from dev_log import db
-from dev_log.models import Absence, Nurse, Care, Office
+import datetime
+from dev_log.models import Absence, Office
 from dev_log.auth.controllers import login_required
+from dev_log.nurses.controllers import edit_nurse
 
 account = Blueprint('account', __name__, url_prefix='/account')
 
 
-@account.route('/edit', methods=['GET', 'POST'])
+@account.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+    print(session.get('office_id'))
+    print(session.get('nurse_id'))
     if request.method == "POST":
         if session.get('office_id') is None:
             id = session['nurse_id']
+            if request.args (edit):
+                edit_nurse(id)
+            elif request.args (vacances):
+                return redirect(url_for('account.add_absence'))
         else:
             id = session['office_id']
-        return redirect(url_for('account.edit_account', id=id))
-    return render_template('account.html')
+            return redirect(url_for('account.edit_account', id=id))
+    return render_template('account.html', id=id)
 
 
 @account.route('/edit', methods=['GET', 'POST'])
 @login_required
 def edit_account(id):
     if session.get('nurse_id') is not None:
-        last_name = request.form['last_name']
-        first_name = request.form['first_name']
-        email = request.form['email']
-        phone = request.form['phone']
-        password = request.form['password']
-        address = request.form['address']
-        office = request.form['office']
-        care = Care.query.all()
-        cares = ""
-        for c in care:
-            if request.form.get(str(c.id)) is not None:
-                cares += "-{}-".format(c.id)
-        error = None
-
-        password = generate_password_hash(password)
-        db.session.query(Nurse).filter(Nurse.id == id). \
-            update(dict(last_name=last_name,
-                        first_name=first_name,
-                        email=email,
-                        phone=phone,
-                        password=password,
-                        address=address))
-        db.session.commit()
-        flash("Your information have been updated")
-
+        edit_nurse(id)
     elif session.get('office_id') is not None:
-        name = request.form ['name']
+        name = request.form['name']
         email = request.form['email']
         phone = request.form['phone']
         password = request.form['password']
@@ -67,10 +49,31 @@ def edit_account(id):
         db.session.commit()
         flash("The office's information have been updated")
 
-    return redirect(url_for('account.home'))
+        return redirect(url_for('account.home'))
 
 
 @account.route('/absence', methods=['GET', 'POST'])
 @login_required
-def absence():
-    pass
+def add_absence(id):
+    if request.method == "POST":
+        # if True: #demie journée
+        #     date = request.form['date']
+        #     halfday = request.form['halfday']
+        #     absence = Absence(nurse_id=id, date=date, halfday=halfday)
+        #     db.session.add(absence)
+        # else: #période
+        if request.args.get('period'):
+            start_date = datetime.datetime.strptime(request.form['start_date'], '%Y-%m-%d').date()
+            end_date = datetime.datetime.strptime(request.form['end_date'], '%Y-%m-%d').date()
+            days_in_period = []
+            if start_date <= end_date:
+                for n in range((end_date - start_date).days + 1):
+                    days_in_period.append(start_date + datetime.timedelta(n))
+            else:
+                for n in range((start_date - end_date).days + 1):
+                    days_in_period.append(start_date - datetime.timedelta(n))
+            for d in days_in_period:
+                for halfday in ['Morning', 'Afternoon']:
+                    absence = Absence(nurse_id=id, date=d, halfday=halfday)
+                    db.session.add(absence)
+    return render_template('add_vacation.html')
