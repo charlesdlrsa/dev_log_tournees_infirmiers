1c88b6d2945838691b067a26c6de05c9ce1bc2d1
charlesdlrsa <charlesdlrsa@hotmail.fr>
2019-01-28 23:41:45 +0100

    Premiers pas vers la page nurse planning

--- dev_log/planning/controllers.py
+++ dev_log/planning/controllers.py
@@ -4,7 +4,6 @@ from datetime import datetime, timedelta
 from dev_log.auth.controllers import login_required
 from dev_log.auth.controllers import admin_required
 from dev_log.utils.calendar import *
-from dev_log.utils.calendar import *
 from dev_log.models import Appointment, Patient, Nurse, Care, Office
 from dev_log import db
 
@@ -12,7 +11,7 @@ planning = Blueprint('planning', __name__, url_prefix='/planning')
 
 
 @planning.route("/", methods=['GET', 'POST'])
-@admin_required
+@login_required
 def home():
     if request.method == "POST":
         nurse_id = request.form['input_nurse']
@@ -36,68 +35,31 @@ def home():
     time = iso_to_gregorian(year, week, day)
     time = time.strftime('%Y-%m-%d')
 
-    nurses = Nurse.query.filter(Nurse.office_id == session['office_id'])
+    if session.get('office_id'):
+        nurses = Nurse.query.filter(Nurse.office_id == session['office_id'])
+    else:
+        nurses = Nurse.query.filter(Nurse.id == session['nurse_id'])
 
-    return render_template("planning.html", nurses=nurses, time=time)
+    return render_template("planning_home.html", nurses=nurses, time=time)
 
 
 @planning.route('/<int:nurse_id>/<date>/<halfday>', methods=['GET', 'POST'])
+@login_required
 def get_nurse_planning(nurse_id, date, halfday):
+
     if request.method == "POST":
-        customer_1 = request.args('customer_1')
-        customer_2 = request.args('customer_2')
+        pass
 
-    customer_1 = None
-    customer_2 = None
-    return render_template("map_planning.html", customer_1=customer_1, customer_2=customer_2)
+    departure = {'lat': 48.81, 'lng': 2.34}
+    arrival = {'lat': 48.83, 'lng': 2.36}
+    travel_mode = 'DRIVING'
 
-
-
-# @planning.route("/", methods=['GET', 'POST'])
-# def home():
-#     if request.method == "POST":
-#         if "date" in request.form.keys():
-#             date = datetime.datetime.strptime(request.form['date'], '%Y-%m-%d').date()
-#         else:
-#             date = None
-#         return redirect(url_for('planning.home',date=date,nurse_research=nurse_research))
-#     # if "week" in request.args:
-#     #     week=int(request.args['week'])
-#     #     year=int(request.args['year'])
-#     #     if week==0:
-#     #         week=52
-#     #         year=year-1
-#     #     elif week==53:
-#     #         week=1
-#     #         year=year+1
-#     if "date" in request.args:
-#         current_date = datetime.datetime.strptime(request.args['date'], '%Y-%m-%d').date()
-#     else:
-#         current_date = datetime.datetime.now()
-#     day = 1
-#     if "nurse_research" in request.args:
-#         if request.args["nurse_research"] != "Choose Nurse":
-#             nurse_research = request.args["nurse_research"]
-#         else:
-#             nurse_research = None
-#     else:
-#         nurse_research = None
-#     ############ Insert filter on appointments
-#     week = current_date.isocalendar()[1]
-#     year = current_date.isocalendar()[0]
-#     time = iso_to_gregorian(year, week, day)
-#     time = time.strftime('%Y-%m-%d')
-#     start_week=iso_to_gregorian(year,week,1)
-#     end_week=iso_to_gregorian(year,week,7)
-#     start_week = str(start_week.day) + '/' + str(start_week.month)
-#     end_week = str(end_week.day) + '/' + str(end_week.month)
-#     nurses = db.session.query(Nurse).all()
-#
-#     return render_template("landing.html", start_week=start_week, end_week=end_week, year=year, week=week, time=time,
-#                            nurses=nurses, nurse_research=nurse_research)
+    return render_template("planning_nurse.html", departure=departure, arrival=arrival, travel_mode=travel_mode,
+                           date=date, halfday=halfday)
 
 
 @planning.route("/init", methods=['GET', 'POST'])
+@admin_required
 def reinit_db():
     init_db()
     return redirect(url_for("planning.home"))
