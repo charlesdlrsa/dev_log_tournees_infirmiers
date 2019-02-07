3527d49fb1f7584a3f1f7da59b19ddcf5e4bb2a7
charlesdlrsa <charlesdlrsa@hotmail.fr>
2019-02-07 16:54:38 +0100

    resolving conflicts

--- dev_log/init_database.py
+++ dev_log/init_database.py
@@ -83,25 +83,25 @@ def init_db():
     db.session.add(Absence(3, datetime.date(2019, 2, 1), "Afternoon"))
 
     for pID in range(1, 5):
-        db.session.add(Appointment(patient_id=pID, date=datetime.date(2019, 5, 5), care_id=random.randint(1, 6),
+        db.session.add(Appointment(patient_id=pID, date=datetime.date(2019, 5, 2), care_id=random.randint(1, 6),
                                    halfday="Morning"))
         # To be deleted
         db.session.add(Schedule(appointment_id=pID, hour=datetime.time(8 + pID-1, 30),
                                 nurse_id=1))
     for pID in range(1, 5):
-        db.session.add(Appointment(patient_id=pID, date=datetime.date(2019, 5, 5), care_id=random.randint(1, 6),
+        db.session.add(Appointment(patient_id=pID, date=datetime.date(2019, 5, 2), care_id=random.randint(1, 6),
                                    halfday="Afternoon"))
         # To be deleted
         db.session.add(Schedule(appointment_id=pID+4, hour=datetime.time(13 + pID-1, 30),
                                 nurse_id=1))
     for pID in range(5, 9):
-        db.session.add(Appointment(patient_id=pID, date=datetime.date(2019, 5, 5), care_id=random.randint(1, 6),
+        db.session.add(Appointment(patient_id=pID, date=datetime.date(2019, 5, 2), care_id=random.randint(1, 6),
                                    halfday="Morning"))
         # To be deleted
         db.session.add(Schedule(appointment_id=pID+4, hour=datetime.time(8 + pID-4-1, 30),
                                 nurse_id=2))
     for pID in range(5, 9):
-        db.session.add(Appointment(patient_id=pID, date=datetime.date(2019, 5, 5), care_id=random.randint(1, 6),
+        db.session.add(Appointment(patient_id=pID, date=datetime.date(2019, 5, 2), care_id=random.randint(1, 6),
                                    halfday="Afternoon"))
         # To be deleted
         db.session.add(Schedule(appointment_id=pID+8, hour=datetime.time(13 + pID-4-1, 30),
